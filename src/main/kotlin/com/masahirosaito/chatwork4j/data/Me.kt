b5bbd57e515f9f63cc2fc6fe4dadeec1fce440a0
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-01-19 12:18:18 +0900

    Me データクラスを作成

---
+++ src/main/kotlin/com/masahirosaito/chatwork4j/data/Me.kt
@@ -0,0 +1,25 @@
+package com.masahirosaito.chatwork4j.data
+
+/**
+ * Created by masahiro on 2017/01/19.
+ */
+data class Me(
+        val account_id: Int,
+        val room_id: Int,
+        val name: String,
+        val chatwork_id: String,
+        val organization_id: Int,
+        val organization_name: String,
+        val department: String,
+        val title: String,
+        val url: String,
+        val introduction: String,
+        val mail: String,
+        val tel_organization: String,
+        val tel_extension: String,
+        val tel_mobile: String,
+        val skype: String,
+        val facebook: String,
+        val twitter: String,
+        val avatar_image_url: String
+)
