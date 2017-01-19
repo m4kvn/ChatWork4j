ce50bcfd5f6138b1934416066cb0c82e66bc0693
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-01-19 13:02:08 +0900

    TaskRoom データクラス作成

---
+++ src/main/kotlin/com/masahirosaito/chatwork4j/data/TaskRoom.kt
@@ -0,0 +1,10 @@
+package com.masahirosaito.chatwork4j.data
+
+/**
+ * Created by masahiro on 2017/01/19.
+ */
+data class TaskRoom(
+        val room_id: Int,
+        val name: String,
+        val icon_path: String
+)
