450fb5a320ee610fc33db7d22c2549cc7bd3c75e
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-01-19 15:11:25 +0900

    Roomのメソッドを移動

--- src/main/kotlin/com/masahirosaito/chatwork4j/ChatWork4j.kt
+++ src/main/kotlin/com/masahirosaito/chatwork4j/ChatWork4j.kt
@@ -2,13 +2,10 @@ package com.masahirosaito.chatwork4j
 
 import com.google.gson.Gson
 import com.masahirosaito.chatwork4j.data.Me
-import com.masahirosaito.chatwork4j.data.rooms.Message
 import com.masahirosaito.chatwork4j.data.rooms.Room
 import com.masahirosaito.chatwork4j.data.contacts.Contact
 import com.masahirosaito.chatwork4j.data.my.Status
 import com.masahirosaito.chatwork4j.data.my.Task
-import com.masahirosaito.chatwork4j.data.rooms.File
-import com.masahirosaito.chatwork4j.data.rooms.Member
 import okhttp3.OkHttpClient
 import okhttp3.Request
 
@@ -16,18 +13,26 @@ import okhttp3.Request
  * Created by masahiro on 2017/01/19.
  */
 class ChatWork4j(val TOKEN: String) {
-    val CHATWORK_API_URL_ROOT = "https://api.chatwork.com/v1"
-    val CHATWORK_API_TOKEN_HEADER = "X-ChatWorkToken"
 
-    val client = OkHttpClient()
+    companion object {
+        val CHATWORK_API_URL_ROOT = "https://api.chatwork.com/v1"
+        val CHATWORK_API_TOKEN_HEADER = "X-ChatWorkToken"
+        var CHATWORK_API_TOKEN: String = ""
 
-    private fun getJsonFromResponse(url: String) : String {
-        val request = Request.Builder()
-                .url(CHATWORK_API_URL_ROOT + url)
-                .addHeader(CHATWORK_API_TOKEN_HEADER, TOKEN)
-                .build()
-        val response = client.newCall(request).execute()
-        return response.body().string()
+        val client = OkHttpClient()
+
+        fun getJsonFromResponse(url: String) : String {
+            val request = Request.Builder()
+                    .url(CHATWORK_API_URL_ROOT + url)
+                    .addHeader(CHATWORK_API_TOKEN_HEADER, CHATWORK_API_TOKEN)
+                    .build()
+            val response = client.newCall(request).execute()
+            return response.body().string()
+        }
+    }
+
+    init {
+        CHATWORK_API_TOKEN = TOKEN
     }
 
     fun getMe() : Me = Gson().fromJson(
@@ -58,40 +63,5 @@ class ChatWork4j(val TOKEN: String) {
     fun getRoom(roomId: Int) : Room = Gson().fromJson(
             getJsonFromResponse("/rooms/$roomId"),
             Room::class.java
-    )
-
-    fun Room.getMembers() : Array<Member> = Gson().fromJson(
-            getJsonFromResponse("/rooms/${this.room_id}/members"),
-            Array<Member>::class.java
-    )
-
-    fun Room.getMessages(force: Int) : Array<Message> = Gson().fromJson(
-            getJsonFromResponse("/rooms/${this.room_id}/messages?force=$force"),
-            Array<Message>::class.java
-    )
-
-    fun Room.getMessage(messageId: Int) : Message = Gson().fromJson(
-            getJsonFromResponse("/rooms/${this.room_id}/messages/$messageId"),
-            Message::class.java
-    )
-
-    fun Room.getTasks() : Array<Task> = Gson().fromJson(
-            getJsonFromResponse("/rooms/${this.room_id}/tasks"),
-            Array<Task>::class.java
-    )
-
-    fun Room.getTask(taskId: Int) : Task = Gson().fromJson(
-            getJsonFromResponse("/rooms/${this.room_id}/tasks/$taskId"),
-            Task::class.java
-    )
-
-    fun Room.getFiles() : Array<File> = Gson().fromJson(
-            getJsonFromResponse("/rooms/${this.room_id}/files"),
-            Array<File>::class.java
-    )
-
-    fun Room.getFile(fileId: Int) : File = Gson().fromJson(
-            getJsonFromResponse("/rooms/${this.room_id}/files/$fileId"),
-            File::class.java
     )
 }
--- src/main/kotlin/com/masahirosaito/chatwork4j/data/rooms/Room.kt
+++ src/main/kotlin/com/masahirosaito/chatwork4j/data/rooms/Room.kt
@@ -1,5 +1,9 @@
 package com.masahirosaito.chatwork4j.data.rooms
 
+import com.google.gson.Gson
+import com.masahirosaito.chatwork4j.ChatWork4j.Companion.getJsonFromResponse
+import com.masahirosaito.chatwork4j.data.my.Task
+
 /**
  * Created by masahiro on 2017/01/19.
  */
@@ -18,4 +22,39 @@ data class Room(
         val icon_path: String,
         val last_update_time: Long,
         val description: String
-)
+) {
+    fun getMembers() : Array<Member> = Gson().fromJson(
+            getJsonFromResponse("/rooms/${this.room_id}/members"),
+            Array<Member>::class.java
+    )
+
+    fun getMessages(force: Int) : Array<Message> = Gson().fromJson(
+            getJsonFromResponse("/rooms/${this.room_id}/messages?force=$force"),
+            Array<Message>::class.java
+    )
+
+    fun getMessage(messageId: Int) : Message = Gson().fromJson(
+            getJsonFromResponse("/rooms/${this.room_id}/messages/$messageId"),
+            Message::class.java
+    )
+
+    fun getTasks() : Array<Task> = Gson().fromJson(
+            getJsonFromResponse("/rooms/${this.room_id}/tasks"),
+            Array<Task>::class.java
+    )
+
+    fun getTask(taskId: Int) : Task = Gson().fromJson(
+            getJsonFromResponse("/rooms/${this.room_id}/tasks/$taskId"),
+            Task::class.java
+    )
+
+    fun getFiles() : Array<File> = Gson().fromJson(
+            getJsonFromResponse("/rooms/${this.room_id}/files"),
+            Array<File>::class.java
+    )
+
+    fun getFile(fileId: Int) : File = Gson().fromJson(
+            getJsonFromResponse("/rooms/${this.room_id}/files/$fileId"),
+            File::class.java
+    )
+}
