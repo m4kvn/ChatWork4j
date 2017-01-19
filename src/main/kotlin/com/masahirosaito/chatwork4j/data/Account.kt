0861657b13c32c64680b5516ffd87084ee9a139b
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-01-19 12:20:17 +0900

    Account データクラスを作成

---
+++ src/main/kotlin/com/masahirosaito/chatwork4j/data/Account.kt
@@ -0,0 +1,10 @@
+package com.masahirosaito.chatwork4j.data
+
+/**
+ * Created by masahiro on 2017/01/19.
+ */
+data class Account(
+        val account_id: Int,
+        val name: String,
+        val avatar_image_url: String
+)
