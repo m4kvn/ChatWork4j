c4ad08686450d00d909299030d2d974100bc3670
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-01-19 13:05:38 +0900

    AssignedByAccount データクラスを作成

---
+++ src/main/kotlin/com/masahirosaito/chatwork4j/data/my/AssignedByAccount.kt
@@ -0,0 +1,10 @@
+package com.masahirosaito.chatwork4j.data.my
+
+/**
+ * Created by masahiro on 2017/01/19.
+ */
+data class AssignedByAccount(
+        val account_id: Int,
+        val name: String,
+        val avatar_image_url: String
+)
